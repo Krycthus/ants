d3c60a67c1e24c5b9c4e311f4f61b8df2ba6c645
Barabé Pierre <darkansloading@gmail.com>
2018-04-20 23:28:55 +0200

    Ajout de networkx pour le graphique

--- ants.py
+++ ants.py
@@ -1,9 +1,14 @@
 import pants
 import math
 import csv
+import matplotlib.pyplot as plt
+import networkx as nx
+import time
+from datetime import timedelta
 from pprint import pprint
 
 #déclaration des variables
+G = nx.Graph()
 nodes = []
 max = 20
 
@@ -15,10 +20,13 @@ with open('open_pubs.csv') as csvfile:
 
 nodes = nodes[0:max]
 
+
+
 #Calcule des distances les plus courtes avec la méthode euclidienne
 def distance (x,y):
     dist = math.sqrt(pow(x[1]-y[1],2) + pow(x[0]-y[0],2))
     return dist
+
 
 #Création du monde
 monde = pants.World(nodes, distance)
@@ -27,5 +35,31 @@ solver = pants.Solver()
 #Résolution du monde
 solution = solver.solve(monde)
 
-#Affichage
-pprint(solution.distance)
+columns = "{!s:<25}\t{:<25}"
+divider = "-" * (25 + 25)
+header = columns.format("Temps passé", "Distance")
+columns = columns.replace('<', '>', 1)
+
+print()
+print(header)
+print(divider)
+
+fastest = None
+start_time = time.time()
+for i, ant in enumerate(solver.solutions(monde)):
+   fastest = ant
+   fastest_time = timedelta(seconds=(time.time() - start_time))
+   print(columns.format(fastest_time, ant.distance))
+total_time = timedelta(seconds=(time.time() - start_time))
+
+print(divider)
+print("Meilleur solution:")
+for i, n in zip(fastest.visited, fastest.tour):
+   print("  {:>8} = {}".format(i, n))
+
+print("Solution : {}".format(fastest.distance))
+print("Trouvé en {} sur {} seconds.".format(fastest_time, total_time))
+
+G.add_nodes_from(nodes)
+nx.draw_networkx(G)
+plt.show()
